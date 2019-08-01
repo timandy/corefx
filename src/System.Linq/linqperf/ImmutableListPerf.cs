115b15f6e052b0a45179f907797ac3eb6fb5bf39
TimAndy <xuchonglei@126.com>
2019-08-01 15:24:51 +0800

    add performance test project

---
+++ src/System.Linq/linqperf/ImmutableListPerf.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using BenchmarkDotNet.Attributes;
+
+namespace linqperf
+{
+    [CoreJob]
+    public class ImmutableTest
+    {
+        private ImmutableList<int> data;
+
+        [Params(10, 100, 1000)]
+        public int length;
+
+        [GlobalSetup]
+        public void Init()
+        {
+            int[] a = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = i;
+            }
+
+            data = ImmutableList.Create(a);
+        }
+
+        [Benchmark]
+        public int Enumerate()
+        {
+            int sum = 0;
+            using (IEnumerator<int> e = data.GetEnumerator())
+            {
+                while (e.MoveNext())
+                    sum += e.Current;
+            }
+
+            return sum;
+        }
+
+        [Benchmark]
+        public int Index()
+        {
+            int sum = 0;
+            ImmutableList<int> dataTmp = data;
+            for (int i = 0, len = length; i < len; i++)
+                sum += dataTmp[i];
+            return sum;
+        }
+    }
+}
---
+++ src/System.Linq/linqperf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using BenchmarkDotNet.Running;
+
+namespace linqperf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+            Console.ReadLine();
+        }
+    }
+}
